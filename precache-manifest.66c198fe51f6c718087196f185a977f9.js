0c7e98af7e9954a63b6a538a95f55d696a969ff1
Luc van Kerkvoort <luc.van.kerkvoort@gmail.com>
2020-08-11 18:33:20 -0700

    Updates

---
+++ precache-manifest.66c198fe51f6c718087196f185a977f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85e16214b12b49141beded0588a2abef",
+    "url": "/kastjesfabriek-admin/index.html"
+  },
+  {
+    "revision": "fb914706da5eaee94bf6",
+    "url": "/kastjesfabriek-admin/static/css/main.9170e7f0.chunk.css"
+  },
+  {
+    "revision": "39d6c2fbfa70e0baa63b",
+    "url": "/kastjesfabriek-admin/static/js/2.acc81240.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/kastjesfabriek-admin/static/js/2.acc81240.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb914706da5eaee94bf6",
+    "url": "/kastjesfabriek-admin/static/js/main.88ea3607.chunk.js"
+  },
+  {
+    "revision": "2cf7ea6df0c6e2ebbeaf",
+    "url": "/kastjesfabriek-admin/static/js/runtime-main.07300053.js"
+  },
+  {
+    "revision": "c09abf50d7ed104075943a27cd8a88ad",
+    "url": "/kastjesfabriek-admin/static/media/IMG-20200720-WA0009.c09abf50.jpg"
+  },
+  {
+    "revision": "e44725f85aa17f9399f07e0e08d45cfe",
+    "url": "/kastjesfabriek-admin/static/media/Logo.e44725f8.png"
+  }
+]);
